f83289d0f9978f63beec2bbca9ea3a3e5ffef774
JuanValdez <juantvaldez85@gmail.com>
2021-11-24 03:16:58 -0500

    fetched api/days for sidebar list of days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'components/Application.scss';
 import DayList from './DayList.js';
-
+import axios from 'axios';
 import InterviewerList from './InterviewerList.js';
 import Appointment from './Appointment/index.js';
 const appointments = {
@@ -42,29 +42,39 @@ const appointments = {
     time: '4pm',
   },
 };
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-];
+// make response like days
+// const days = [
+//   {
+//     id: 1,
+//     name: 'Monday',
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: 'Tuesday',
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: 'Wednesday',
+//     spots: 0,
+//   },
+// ];
 
-// const appointment = Object.values(appointments).map();
-
-// console.log('Appointment Array: ' + appointment);
 export default function Application(props) {
+  // const [day, setDay] = useState('Monday');
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+  const url = `http://localhost:8001/api/days`;
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setDays([...response.data]);
+    });
+  }, []);
+
+  // console.log('Day: ', day);
+  // console.log('Days: ', days);
 
   const changeDay = function (newDay) {
     if (day === 'Monday') {
@@ -72,6 +82,10 @@ export default function Application(props) {
     } else if (day === 'Tuesday') {
       setDay(newDay);
     } else if (day === 'Wednesday') {
+      setDay(newDay);
+    } else if (day === 'Thursday') {
+      setDay(newDay);
+    } else if (day === 'Friday') {
       setDay(newDay);
     }
   };
@@ -102,7 +116,6 @@ export default function Application(props) {
           src='images/lhl.png'
           alt='Lighthouse Labs'
         />
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className='schedule'>
         {Object.values(appointments).map((appointment) => {
@@ -116,8 +129,296 @@ export default function Application(props) {
           );
           return <Appointment key='last' time='5pm' />;
         })}
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
   );
 }
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import 'components/Application.scss';
+// import DayList from './DayList.js';
+
+// import InterviewerList from './InterviewerList.js';
+// import Appointment from './Appointment/index.js';
+// const appointments = {
+//   1: {
+//     id: 1,
+//     time: '12pm',
+//   },
+//   2: {
+//     id: 2,
+//     time: '1pm',
+//     interview: {
+//       student: 'Lydia Miller-Jones',
+//       interviewer: {
+//         id: 3,
+//         name: 'Sylvia Palmer',
+//         avatar: 'https://i.imgur.com/LpaY82x.png',
+//       },
+//     },
+//   },
+//   3: {
+//     id: 3,
+//     time: '2pm',
+//   },
+//   4: {
+//     id: 4,
+//     time: '3pm',
+//     interview: {
+//       student: 'Archie Andrews',
+//       interviewer: {
+//         id: 4,
+//         name: 'Cohana Roy',
+//         avatar: 'https://i.imgur.com/FK8V841.jpg',
+//       },
+//     },
+//   },
+//   5: {
+//     id: 5,
+//     time: '4pm',
+//   },
+// };
+// const days = [
+//   {
+//     id: 1,
+//     name: 'Monday',
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: 'Tuesday',
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: 'Wednesday',
+//     spots: 0,
+//   },
+// ];
+
+// export default function Application(props) {
+//   const [fetchedData, setFetchedData] = useState([]);
+//   const [day, setDay] = useState('Monday');
+
+//   const changeDay = function (newDay) {
+//     if (day === 'Monday') {
+//       setDay(newDay);
+//     } else if (day === 'Tuesday') {
+//       setDay(newDay);
+//     } else if (day === 'Wednesday') {
+//       setDay(newDay);
+//     }
+//   };
+
+//   const url = `http://localhost:8001/api/days`;
+
+//   const fetchApi = async () => {};
+
+// useEffect(() => {
+//   axios.get(url).then((response) => {
+//     // const apiData = response;
+
+//     // const fetched = apiData.data.map((day) => {
+//     //   console.log('New Day: ', day);
+//     //   setFetchedData(day);
+//     // });
+
+//     // apiData.data.map((day) => {
+//     //   console.log('New Day: ', day);
+//     // });
+
+//     console.log('Fetched: ', setFetchedData([...response.data.fetchedData]));
+//     // console.log('Fetched: ', [fetchedData]);
+//     // console.log('Data 2: ', [apiData.data[0]]);
+//     // console.log('Days Array: ', days);
+//   });
+// }, []);
+
+//   return (
+//     <main className='layout'>
+//       <section className='sidebar'>
+//         <img
+//           className='sidebar--centered'
+//           src='images/logo.png'
+//           alt='Interview Scheduler'
+//         />
+//         <hr className='sidebar__separator sidebar--centered' />
+//         <nav className='sidebar__menu'>
+//           {/* {apiData.data.map((day) => {
+//             console.log('New Day: ', day);
+//           })}
+//           ; */}
+//           <DayList
+//             days={days}
+//             value={day}
+//             //  onChange={(day) => {
+//             //    changeDay(day);
+//             //   }}
+//             // setDay={(day) => {
+//             //   changeDay(day);
+//             // }}
+//           />
+//         </nav>
+//         <img
+//           className='sidebar__lhl sidebar--centered'
+//           src='images/lhl.png'
+//           alt='Lighthouse Labs'
+//         />
+//       </section>
+//       <section className='schedule'>
+//         {Object.values(appointments).map((appointment) => {
+//           return (
+//             <Appointment
+//               key={appointment.id}
+//               id={appointment.id}
+//               time={appointment.time}
+//               interview={appointment.interview}
+//             />
+//           );
+//           return <Appointment key='last' time='5pm' />;
+//         })}
+//       </section>
+//     </main>
+//   );
+// }
+
+// // import React, { useState, useEffect } from 'react';
+// // import 'components/Application.scss';
+// // import DayList from './DayList.js';
+// // import axios from 'axios';
+// // import InterviewerList from './InterviewerList.js';
+// // import Appointment from './Appointment/index.js';
+// // const appointments = {
+// //   1: {
+// //     id: 1,
+// //     time: '12pm',
+// //   },
+// //   2: {
+// //     id: 2,
+// //     time: '1pm',
+// //     interview: {
+// //       student: 'Lydia Miller-Jones',
+// //       interviewer: {
+// //         id: 3,
+// //         name: 'Sylvia Palmer',
+// //         avatar: 'https://i.imgur.com/LpaY82x.png',
+// //       },
+// //     },
+// //   },
+// //   3: {
+// //     id: 3,
+// //     time: '2pm',
+// //   },
+// //   4: {
+// //     id: 4,
+// //     time: '3pm',
+// //     interview: {
+// //       student: 'Archie Andrews',
+// //       interviewer: {
+// //         id: 4,
+// //         name: 'Cohana Roy',
+// //         avatar: 'https://i.imgur.com/FK8V841.jpg',
+// //       },
+// //     },
+// //   },
+// //   5: {
+// //     id: 5,
+// //     time: '4pm',
+// //   },
+// // };
+
+// // // const days = [
+// // //   {
+// // //     id: 1,
+// // //     name: 'Monday',
+// // //     spots: 2,
+// // //   },
+// // //   {
+// // //     id: 2,
+// // //     name: 'Tuesday',
+// // //     spots: 5,
+// // //   },
+// // //   {
+// // //     id: 3,
+// // //     name: 'Wednesday',
+// // //     spots: 0,
+// // //   },
+// // // ];
+
+// // export default function Application(props) {
+// //   // const [day, setDay] = useState('Monday');
+// //   const [days, setDays] = useState([]);
+
+// //   const changeDay = function (newDay) {
+// //     if (days === 'Monday') {
+// //       setDays(newDay);
+// //     } else if (days === 'Tuesday') {
+// //       setDays(newDay);
+// //     } else if (days === 'Wednesday') {
+// //       setDays(newDay);
+// //     } else if (days === 'Thursday') {
+// //       setDays(newDay);
+// //     } else if (days === 'Friday') {
+// //       setDays(newDay);
+// //     }
+// //   };
+
+// // useEffect(() => {
+// //   const url = `http://localhost:8001/api/days`;
+// //   axios.get(url).then((response) => {
+// //     const fetchedResults = response;
+// //     // console.log([response.data[0].name]);
+// //     setDays([fetchedResults]);
+// //     console.log('Days: ' + [days]);
+// //     console.log('Fetched Data: ', [fetchedResults]);
+// //   });
+// // }, []);
+
+// //   const fetchApI = async () => {
+
+// //   }
+
+// //   return (
+// //     <main className='layout'>
+// //       <section className='sidebar'>
+// //         <img
+// //           className='sidebar--centered'
+// //           src='images/logo.png'
+// //           alt='Interview Scheduler'
+// //         />
+// //         <hr className='sidebar__separator sidebar--centered' />
+// //         <nav className='sidebar__menu'>
+// //           <DayList
+// //             days={days}
+// //             value={days}
+// //             onChange={(day) => {
+// //               changeDay(day);
+// //             }}
+// //             // setDay={(day) => {
+// //             //   changeDay(day);
+// //             // }}
+// //           />
+// //         </nav>
+// //         <img
+// //           className='sidebar__lhl sidebar--centered'
+// //           src='images/lhl.png'
+// //           alt='Lighthouse Labs'
+// //         />
+// //       </section>
+// //       <section className='schedule'>
+// //         {Object.values(appointments).map((appointment) => {
+// //           return (
+// //             <Appointment
+// //               key={appointment.id}
+// //               id={appointment.id}
+// //               time={appointment.time}
+// //               interview={appointment.interview}
+// //             />
+// //           );
+// //           return <Appointment key='last' time='5pm' />;
+// //         })}
+// //       </section>
+// //     </main>
+// //   );
+// // }
