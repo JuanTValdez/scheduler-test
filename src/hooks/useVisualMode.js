dab794fc0bec7d6e239eb5d3502d204ac4ef4ce4
JuanValdez <juantvaldez85@gmail.com>
2021-12-07 23:26:16 -0500

    completed useVisualHook for setting modes and history of all modes traveled to

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(newMode) {
+    setMode(newMode);
+    setHistory([...history, newMode]);
+    console.log('History after push: ' + history);
+  }
+
+  function back() {
+    if (history.length >= 1) {
+      console.log('before POP: ' + history);
+      history.pop();
+
+      setHistory(
+        history
+        // (history) => history.filter((_, pop) => pop !== history.length - 1)
+      );
+    }
+    console.log('History after pop: ' + history);
+    console.log('Mode before setMode: ' + mode);
+    setMode(history[history.length - 1]);
+    console.log('Mode after setMode: ' + mode);
+  }
+
+  return { mode, history, transition, back };
+  // May also be written:
+  // return {mode: mode}
+  //it is the same as the current return
+}
