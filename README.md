7aa9e5ce8f7c3e7ceabda723c7fe59de7b60f30e
JuanValdez <juantvaldez85@gmail.com>
2022-01-06 16:53:03 -0500

    Created README.md

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ Install dependencies with `npm install`.
 
 ```sh
 npm start
+
+In the .env.development file, it may be required to change the PORT to 8000.
+Additionally, inside the package.json file, you may need to change "proxy": "http://localhost:8007" to "proxy": "http://localhost:8001".
+
+You may also need to uncomment axios lines in the index.js file.
 ```
 
 ## Running Jest Test Framework
@@ -21,3 +26,44 @@ npm test
 ```sh
 npm run storybook
 ```
+
+## devDependencies
+
+- @babel/core: ^7.4.3,
+- @storybook/addon-actions: ^5.0.10,
+- @storybook/addon-backgrounds: ^5.0.10,
+- @storybook/addon-links: ^5.0.10,
+- @storybook/addons: ^5.0.10,
+- @storybook/react: ^5.0.10,
+- @testing-library/jest-dom: ^4.0.0,
+- @testing-library/react": ^8.0.7",
+- @testing-library/react-hooks: ^7.0.2,
+- babel-loader: ^8.0.5,
+- node-sass: ^4.14.0",
+- react-test-renderer: ^16.9.0
+
+## Screenshots
+
+### Homepage
+
+![Desktop Version](docs/homepage.png)
+
+### Booking appointment
+
+![Desktop Version](docs/create_booking.png)
+
+### Saving appointment
+
+![Desktop Version](docs/saving.png)
+
+### After booking appointment
+
+![Desktop Version](docs/booked_appointment.png)
+
+### Deleting appointment
+
+![Desktop Version](docs/deleting.png)
+
+### Deleting Confirmation
+
+![Desktop Version](docs/delete_confirm.png)
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,41 +1,64 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-export default function useVisualMode(initialMode) {
-  const [mode, setMode] = useState(initialMode);
-  const [history, setHistory] = useState([initialMode]);
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
 
-  function transition(newMode, replace = false) {
-    if (replace === true) {
-      // history.pop();
-
-      // setHistory(history);
-      setHistory((prev) => [...prev, mode]);
-      // setMode(history[history.length - 1]);
+  function transition(newMode, replaceLastMode = false) {
+    if (replaceLastMode) {
+      setHistory((prev) => [...prev.slice(0, prev.length - 1), newMode]);
+    } else {
+      // setHistory([...history, newMode]);
+      setHistory((prev) => [...prev, newMode]);
     }
-
-    setMode(newMode);
-    // setHistory([...history, newMode]);
-
-    // else {
-    //   setMode(newMode);
-    //   setHistory([...history, newMode]);
-    // }
-
-    // console.log('History after push: ' + history);
   }
-
   function back() {
-    if (history.length >= 1) {
-      history.pop();
+    if (history.length < 2) return;
 
-      setHistory(history);
-    }
-
-    setMode(history[0]);
+    // remove last history item
+    setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
   }
-
-  return { mode, history, transition, back };
-  // May also be written:
-  // return {mode: mode}
-  //it is the same as the current return
+  console.log('Mode: ', history);
+  return {
+    mode: history[history.length - 1],
+    transition,
+    back,
+  };
 }
+
+// import React, { useState } from 'react';
+
+// export default function useVisualMode(initialMode) {
+//   const [mode, setMode] = useState(initialMode);
+//   const [history, setHistory] = useState([initialMode]);
+
+//   function transition(newMode, replace = false) {
+//     if (replace === true) {
+//       // history.pop();
+
+//       // setHistory(history);
+//       setHistory((prev) => [...prev, mode]);
+//       // setMode(history[history.length - 1]);
+//     }else{
+
+//     }
+
+//     setMode(newMode);
+//   }
+
+//   function back() {
+//     if (history.length > 1) {
+//       // history.pop();
+
+//       // setHistory(history);
+//       setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
+//     }
+
+//     setMode(history[0]);
+//   }
+
+//   console.log('mode: ', mode, history);
+//   return { mode, history, transition, back };
+//   // May also be written:
+//   // return {mode: mode}
+//   //it is the same as the current return
+// }
